05549cb5a19d54aacb99611331297a7b1cfaedd2
yermaka <ermak.080@mail.ru>
2025-11-05 01:57:09 +0800

    add headless option to firefox driver

--- conftest.py
+++ conftest.py
@@ -1,10 +1,13 @@
 import pytest
 from selenium import webdriver
+from selenium.webdriver.firefox.options import Options
 
 
 @pytest.fixture()
 def driver():
-    browser = webdriver.Firefox()
+    options = Options()
+    options.add_argument("--headless")
+    browser = webdriver.Firefox(options=options)
     browser.maximize_window()
     browser.implicitly_wait(3)  # неявное ожидание в течение 3 секунд
     yield browser
