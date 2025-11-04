304db58e535b2cbd0f608609de15c85fdf679d46
yermaka <ermak.080@mail.ru>
2025-11-05 01:02:47 +0800

    add basic tests and fixture

---
+++ test_demo.py
@@ -0,0 +1,20 @@
+import pytest
+
+
+@pytest.fixture()
+def before_after():
+    print("before test")
+    yield
+    print("\nafter test")
+
+
+def test_demo1(before_after):
+    assert 2 == 2
+
+
+def test_demo2():
+    assert 1 == 1
+
+
+def test_demo3(before_after):
+    assert 2 == 3
